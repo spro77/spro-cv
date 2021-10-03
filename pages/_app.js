fe09f6b56ae8c6930588798604a0894d30339259
Sergey <Prostov>
2021-10-03 16:29:15 +0300

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import 'tailwindcss/tailwind.css'
+import Navigation from "../components/Navigation"
+import Footer from "../components/Footer"
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider attribute="class" enableSystem={false}>
+            <Navigation />
+            <Component {...pageProps} />
+            <Footer />
+          </ThemeProvider>
 }
 
 export default MyApp
